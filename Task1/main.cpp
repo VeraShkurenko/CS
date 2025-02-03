596a90afacc1270a8f9e520cb6211f3590bd5a79
VeraShkurenko <shkurenko.verra>
2025-02-03 11:40:39 +0200

    changes

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -4,13 +4,22 @@
 
 using namespace std;
 
+bool ISPAL(_TCHAR* str) {
+	for ( i = 0; i < _ts; i++)
+	{
+
+	}
+
+}
+
+
 void main()
 {
-	_TCHAR szbuf3[15] = _TEXT("Hello,");
-	_tcscat(szbuf3, _TEXT("world!"));
-	wcout << szbuf3 << '\n';
-	cout << "The size of array: " << sizeof(szbuf3) << "bytes]n";
+	//_TCHAR szbuf3[15] = _TEXT("Hello,");
+	//_tcscat(szbuf3, _TEXT("world!"));
+	//wcout << szbuf3 << '\n';
+	//cout << "The size of array: " << sizeof(szbuf3) << "bytes]n";
 
-
+	_TCHAR str[10] = _TEXT("bob");
 
 }
