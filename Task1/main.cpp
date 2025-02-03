03c126c7418c882b076c1faef6cbebf22e5c70f8
VeraShkurenko <shkurenko.verra>
2025-02-03 11:30:22 +0200

    new changes

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,1 +1,16 @@
+#define _CAT_SECURE_N0_WARNING
 #include <iostream>
+#include <tchar.h>
+
+using namespace std;
+
+void main()
+{
+	_TCHAR szbuf3[15] = _TEXT("Hello,");
+	_tcscat(szbuf3, _TEXT("world!"));
+	wcout << szbuf3 << '\n';
+	cout << "The size of array: " << sizeof(szbuf3) << "bytes]n";
+
+
+
+}
